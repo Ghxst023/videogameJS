d3b70447aa3999ac8ae1daeec5a0ebd859423518
JASA4 <JASA4@softtek.com>
2020-03-09 22:11:35 -0600

    nav added, pages added, fixed nav, under construction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { MyNavComponent } from './components/my-nav/my-nav.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch:'full' },
+    {path: 'home', component: HomeComponent },
+    {path: 'register', component: RegisterComponent },
+    {path: 'my-nav', component: MyNavComponent },
+    {path: 'notfound', component: NotfoundComponent },
+    {path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
