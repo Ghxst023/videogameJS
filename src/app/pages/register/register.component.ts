c0c3bdffaa45d924af1b9cf155fed7ddde57935f
JASA4 <JASA4@softtek.com>
2020-03-12 00:34:26 -0600

    building form w grid system, frontE details fixed and updated

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+    
+    // Titles
+    cardTitle = 'Personal information';
 
   constructor() { }
 
